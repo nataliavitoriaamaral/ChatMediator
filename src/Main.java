e3add42c405148926956dfbf9a8bd9adbb6b6d12
Natália Amaral <natalia.amaraldoval@gmail.com>
2026-05-15 14:56:45 -0300

    feat: inicia classe Main com instanciacao e registro de usuarios

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criacao do Mediator
+        IChatMediator salaDeChat = new ChatMediator();
+
+        // Criacao dos usuarios
+        Usuario user1 = new UsuarioConcreto(salaDeChat, "Alice");
+        Usuario user2 = new UsuarioConcreto(salaDeChat, "Bob");
+        Usuario user3 = new UsuarioConcreto(salaDeChat, "Charlie");
+
+        // Registrando na sala
+        salaDeChat.registrarUsuario(user1);
+        salaDeChat.registrarUsuario(user2);
+        salaDeChat.registrarUsuario(user3);
+    }
+}
